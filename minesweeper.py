c1458e70e3c59cbb1c4cabafacbb44092020cb25
mondeendeguise <mondeendeguise@gmail.com>
2022-03-21 06:19:57 -0600

    10x10 grid with tiles representing values

--- minesweeper.py
+++ minesweeper.py
@@ -9,11 +9,29 @@ pygame.init()
 # CONSTANTS
 
 
-WIDTH, HEIGHT = 600, 400
+WIDTH, HEIGHT = 255, 255
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 clock = pygame.time.Clock()
 FPS = 60
+
+FIELD_S = 10
+
+BOX_MES = 20
+MARGIN = 5
+
+WHITE = [255, 255, 255]
+RED = [255, 0, 0]
+GREY = [128, 128, 128]
+
+FLAG = pygame.image.load(os.path.join('assets', 'img', 'flag.png'))
+
+# create 2d array of 0s
+grid = []
+for row in range(FIELD_S):
+    grid.append([])
+    for column in range(FIELD_S):
+        grid[row].append(0)
 
 
 # FUNCTIONS
@@ -22,7 +40,17 @@ FPS = 60
 def main():
     run = True
     while run:
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+        for row in range(FIELD_S):
+            for column in range(FIELD_S):
+                color = WHITE
+                pygame.draw.rect(screen, color, [(MARGIN + BOX_MES) * column + MARGIN, (MARGIN + BOX_MES) * row + MARGIN, BOX_MES, BOX_MES])
+
+        pygame.display.flip()
+        clock.tick(FPS)
     pygame.quit()
     sys.exit("End of the line, bucko")
 
