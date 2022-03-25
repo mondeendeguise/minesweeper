b77019e232b7f2e0d03b9d5d6f5e06d03f3cb1bc
mondeendeguise <mondeendeguise@gmail.com>
2022-03-24 23:18:46 -0600

    Working grid displayed. Turns grey when you click it

--- minesweeper.py
+++ minesweeper.py
@@ -1,38 +1,53 @@
-import sys
-import os
-import random
+import numpy as np
 import pygame
-
-pygame.init()
-
-
-# CONSTANTS
-
-
-WIDTH, HEIGHT = 255, 255
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
-clock = pygame.time.Clock()
-FPS = 60
-
-WHITE = [255, 255, 255]
-RED = [255, 0, 0]
-GREY = [128, 128, 128]
-
-FLAG = pygame.image.load(os.path.join('assets', 'img', 'flag.png'))
+import random
+import os
+import sys
 
 
 # FUNCTIONS
 
 
-def main():
+def game():
+    pygame.init()
+
+    WIDTH, HEIGHT = 500, 500
+    FPS = 15
+
+    TILES = 25
+    SIZE = 20
+    MARGIN = 5
+
+    WHITE = [255, 255, 255]
+    RED = [255, 0, 0]
+    GREY = [128, 128, 128]
+
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    clock = pygame.time.Clock()
+
+    grid = np.zeros((TILES, TILES), dtype=int)
+
     run = True
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False
-                break
-        pygame.display.flip()
+                pygame.quit()
+                sys.exit("User closed window")
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                clicked = []
+                clicked.append(pygame.mouse.get_pos()[1] // TILES)
+                clicked.append(pygame.mouse.get_pos()[0] // TILES)
+                print(clicked)
+                grid[clicked[0], clicked[1]] = 1
+        # draw grid
+        for row in range(TILES):
+            for column in range(TILES):
+                if grid[row, column] == 0:
+                    color = WHITE
+                elif grid[row, column] == 1:
+                    color = GREY
+                pygame.draw.rect(screen, color, [(MARGIN + SIZE) * column + MARGIN, (MARGIN + SIZE) * row + MARGIN, SIZE, SIZE])
+        pygame.display.update()
         clock.tick(FPS)
     pygame.quit()
     sys.exit("Reached end of sequence")
@@ -42,4 +57,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    game()
