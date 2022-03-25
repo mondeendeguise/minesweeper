5da5bc7837b89ae1f2e3abc3d757b3312b3ac6f2
mondeendeguise <mondeendeguise@gmail.com>
2022-03-24 19:05:33 -0600

    using numpy instead of lengthy for loops

--- minesweeper.py
+++ minesweeper.py
@@ -15,23 +15,11 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 FPS = 60
 
-FIELD_S = 10
-
-BOX_MES = 20
-MARGIN = 5
-
 WHITE = [255, 255, 255]
 RED = [255, 0, 0]
 GREY = [128, 128, 128]
 
 FLAG = pygame.image.load(os.path.join('assets', 'img', 'flag.png'))
-
-# create 2d array of 0s
-grid = []
-for row in range(FIELD_S):
-    grid.append([])
-    for column in range(FIELD_S):
-        grid[row].append(0)
 
 
 # FUNCTIONS
@@ -44,15 +32,10 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 break
-        for row in range(FIELD_S):
-            for column in range(FIELD_S):
-                color = WHITE
-                pygame.draw.rect(screen, color, [(MARGIN + BOX_MES) * column + MARGIN, (MARGIN + BOX_MES) * row + MARGIN, BOX_MES, BOX_MES])
-
         pygame.display.flip()
         clock.tick(FPS)
     pygame.quit()
-    sys.exit("End of the line, bucko")
+    sys.exit("Reached end of sequence")
 
 
 # INITIALIZE
